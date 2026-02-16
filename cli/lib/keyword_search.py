e60f09873406e859a27f5696a8ef5584069f52f3
glebson1988 <glebson1988@yandex.ru>
2026-02-16 17:12:26 +0200

    Term Frequency Saturation

--- cli/lib/keyword_search.py
+++ cli/lib/keyword_search.py
@@ -3,6 +3,7 @@ import pickle
 import math
 from collections import Counter, defaultdict
 
+from .constants import BM25_K1
 from .search_utils import CACHE_DIR, load_movies
 from .search_utils import tokenize_text
 
@@ -93,8 +94,18 @@ class InvertedIndex:
             + 1
         )
 
+    def get_bm25_tf(self, doc_id: int, term: str, k1: float = BM25_K1) -> float:
+        tf = self.get_tf(doc_id, term)
+        return (tf * (k1 + 1)) / (tf + k1)
+
 
 def bm25_idf_command(term: str) -> float:
     index = InvertedIndex()
     index.load()
     return index.get_bm25_idf(term)
+
+
+def bm25_tf_command(doc_id: int, term: str, k1: float = BM25_K1) -> float:
+    index = InvertedIndex()
+    index.load()
+    return index.get_bm25_tf(doc_id, term, k1)
